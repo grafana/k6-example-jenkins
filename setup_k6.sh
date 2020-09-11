cdfb49647db4f535ff96f834344f0ce42e9b06c9
Mostafa Moradian <mostafa@loadimpact.com>
2020-09-11 11:43:57 +0200

    Fix k6 setup script

--- setup_k6.sh
+++ setup_k6.sh
@@ -2,12 +2,12 @@
 set -ex
 
 install_k6() {
-    apt-get update
-    apt-get install dirmngr --install-recommends
-    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+    sudo apt-get update
+    sudo apt-get install dirmngr --install-recommends
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
     echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-    apt-get update
-    apt-get install k6
+    sudo apt-get update
+    sudo apt-get install k6
 }
 
 main() {
